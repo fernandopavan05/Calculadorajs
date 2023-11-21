351eeac6f2caaa08550b677449f620cac7e192da
fernandopavan05 <140528717+fernandopavan05@users.noreply.github.com>
2023-11-21 11:21:42 -0300

    Add files via upload

---
+++ Js.alura/Aula 1 - Number.js
@@ -0,0 +1,18 @@
+// Tipo Number
+
+const num1 = 1
+const num2 = 4
+
+const resultado = num1 + num2;
+
+console.log(resultado)
+
+const numeroPontoFlutuante = 3.3
+const PontoFlutuante = .5
+
+const novaOperacao = num1 / numeroPontoFlutuante
+
+console.log(novaOperacao)
+
+const alura = "Alura";
+console.log(alura +num1)
